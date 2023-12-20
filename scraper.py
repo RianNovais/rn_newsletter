6af3f597c1d7b02d968962e38d7343c892d684a5
Rian Novais <riannovais3@gmail.com>
2023-12-20 11:03:43 -0300

    add scraping "terra" and "cnn" websites

--- scraper.py
+++ scraper.py
@@ -8,12 +8,14 @@ class NewsScraper():
         print(self.currentDate)
         self.g1_url = 'https://g1.globo.com/'
         self.uol_url = 'https://www.uol.com.br/'
-        self.cnn_url = 'https://www.cnnbrasil.com.br/'
+        self.cnn_url = 'https://www.cnnbrasil.com.br/ultimas-noticias/'
         self.terra_url = 'https://www.terra.com.br/noticias/'
 
-        # data = self.scrape_g1(self.g1_url)
+        data = self.scrape_g1(self.g1_url)
         data2 = self.scrape_uol(self.uol_url)
-        
+        data3 = self.scrape_terra(self.terra_url)
+        data4 = self.scrape_cnn(self.cnn_url)
+    
     #method that performs scraping on the "g1" website
     def scrape_g1(self, url):
         data = []
@@ -67,7 +69,6 @@ class NewsScraper():
         for i, news in enumerate(newsList, start=1):
             if i>3:
                 print('Scrapping Uol sucessfully')
-                print(data)
                 return data
             title = news.find('h3', attrs={'class': 'title__element headlineSub__content__title'})
             link = news['href']
@@ -77,7 +78,70 @@ class NewsScraper():
 
             #get the title and link and add it to the data variable
             data.append([title, link])
+    
+    #method that performs scraping on the "terra" website
+    def scrape_terra(self, url):
+        data = []
+        response = requests.get(self.terra_url)
+        if response.status_code != 200:
+            print('ERROR')
+            return
         
+        response_content = response.text
+
+        siteSoup = BeautifulSoup(response_content, 'html.parser')
+        #filtering by the container with the news
+        containerNews = siteSoup.find('div', attrs={'class': 'table-range-grid-step app-t360-subject-table__rect--grid'})
+
+        #in this container we take the element that every news item has, and we do a findAll to get all the news items
+
+        newsList = containerNews.findAll('div', attrs={'class': 'card-news__image'})
+        
+        #for each news item in the list of news items that we took from theNews container using findAll on the specific element, we will now take
+        #the title and link of each news item
+        #put an enumerate to get only the first 3 news items
+        for i, news in enumerate(newsList, start=1):
+            if i>3:
+                print('Terra scrapping sucessfully')
+                
+                return data
+            
+            title = news.find('img')['alt']
+            link = news.find('a', attrs={'class': 'card-news__url'})['href']
+
+            #add the title and link of the news item to the date variable
+            data.append([title, link])
+
+    #method that performs scraping on the "cnn" website
+    def scrape_cnn(self, url):
+        data = []
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            print('ERROR')
+            return 
+        
+        response_content = response.text
+
+        siteSoup = BeautifulSoup(response_content, 'html.parser')
+
+        #pegando from beautifulSoup on our website, the container that has the news
+        containerNews = siteSoup.find('div', attrs={'class': 'posts col__list'})
+        
+        # fetching each news item from the container, and from this list that will come from findAll, we will iterate and get the link and title of each news item
+        newsList = containerNews.findAll('li', attrs={'class': 'home__list__item'})
+
+        #mechanism with enumerate to pick up only the first 3 news items
+        for i, news in enumerate(newsList, start=1):
+            if i>3:
+                print('CNN scrapping sucessfully')
+                print(data)
+                return data
+
+            link = news.find('a', attrs={'class': 'home__list__tag'})['href']
+            titulo = news.find('h3', attrs={'class': 'news-item-header__title market__new__title'}).text
+
+            data.append([titulo, link])
     
         
     
